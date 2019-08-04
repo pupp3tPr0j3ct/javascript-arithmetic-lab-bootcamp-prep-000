03762f15b4b2249466a12878e58a392c37121e92
Jason Santana <jsantana006@gmail.com>
2019-08-04 18:27:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function subtract(a, b){
   var sub = a -= b;
   return sub;
 }
+function multiply(a, b){
+  var mult = a *= b;
+  return mult;
+}
+function divide(a, b){
+  var div = a /= b;
+  return div;
+}
+function inc(n){
+  var increase = n++;
+  return increase;
+}
+function dec(n){
+  var decrease = n--;
+  return decrease;
+}
